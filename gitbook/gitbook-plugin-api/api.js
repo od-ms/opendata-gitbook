26c82fca9325e943ea2b076694a699026ece4b51
od-ms <od-ms@users.noreply.github.com>
2021-09-08 21:21:01 +0000

    Updated by gitbook-action 2021-09-08 21:19:57

--- gitbook/gitbook-plugin-api/api.js
+++
@@ -1,7 +0,0 @@
-require(["gitbook"], function(gitbook) {
-  gitbook.events.bind("page.change", function() {
-    $(".api-header").click(function(event) {
-      $(event.currentTarget).parent().toggleClass('expanded');
-    });
-  });
-});
